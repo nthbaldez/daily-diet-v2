4daeb6ddeb6372eaa3c9ccd2739049542d652e1a
Nathan Baldez <nathanbsb2@hotmail.com>
2025-02-20 09:33:47 -0300

    Initial Commit

---
+++ src/routes/meals.routes.ts
@@ -0,0 +1,35 @@
+import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
+import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-if-session-id-exists'
+
+export async function mealsRoutes(app: FastifyInstance) {
+  app.post(
+    '/',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const mealsBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.coerce.date(),
+      })
+
+      const { name, description, isOnDiet } = mealsBodySchema.parse(
+        request.body,
+      )
+
+      await knex('meals').insert({
+        id: randomUUID(),
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        user_id: request.user?.id,
+        date: Date.now(),
+      })
+
+      return reply.status(201).send()
+    },
+  )
+}
