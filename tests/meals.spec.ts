795e79eeec356ef3b4004f27fc9b92dc459e1f81
Nathan Baldez <nathanbsb2@hotmail.com>
2025-02-20 23:46:14 -0300

    feature: teste para verificar se é possível resgatar uma única refeição do usuário

--- tests/meals.spec.ts
+++ tests/meals.spec.ts
@@ -77,4 +77,106 @@ describe('Meals routes', () => {
       })
       .expect(204)
   })
+
+  it('should be able to delete a specific meal from an user', async () => {
+    const createUserResponse = await request(app.server).post('/users').send({
+      name: 'Nathan Baldez',
+      email: 'nathanbsb2@hotmail.com',
+    })
+
+    const cookies = createUserResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies as string[])
+      .send({
+        name: 'Café da manhã',
+        description:
+          'Panqueca de aveia com banana e mel + café preto sem açúcar',
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(201)
+
+    const mealsResponseBody = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies as string[])
+      .expect(200)
+
+    const mealId = mealsResponseBody.body.meals[0].id
+
+    await request(app.server)
+      .delete(`/meals/${mealId}`)
+      .set('Cookie', cookies as string[])
+      .expect(204)
+  })
+
+  it('should be able to list all meals from an user', async () => {
+    const createUserResponse = await request(app.server).post('/users').send({
+      name: 'Nathan Baldez',
+      email: 'nathanbsb2@hotmail.com',
+    })
+
+    const cookies = createUserResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies as string[])
+      .send({
+        name: 'Café da manhã',
+        description:
+          'Panqueca de aveia com banana e mel + café preto sem açúcar',
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(201)
+
+    await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies as string[])
+      .expect(200)
+  })
+
+  it('should be able to get a specific meal from an user', async () => {
+    const createUserResponse = await request(app.server).post('/users').send({
+      name: 'Nathan Baldez',
+      email: 'nathanbsb2@hotmail.com',
+    })
+
+    const cookies = createUserResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookies as string[])
+      .send({
+        name: 'Café da manhã',
+        description:
+          'Panqueca de aveia com banana e mel + café preto sem açúcar',
+        isOnDiet: true,
+        date: new Date(),
+      })
+      .expect(201)
+
+    const mealsResponse = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookies as string[])
+      .expect(200)
+
+    const mealId = mealsResponse.body.meals[0].id
+
+    const responseMeal = await request(app.server)
+      .get(`/meals/${mealId}`)
+      .set('Cookie', cookies as string[])
+      .expect(200)
+
+    expect(responseMeal.body).toEqual({
+      meal: expect.objectContaining({
+        name: 'Café da manhã',
+        description:
+          'Panqueca de aveia com banana e mel + café preto sem açúcar',
+        is_on_diet: 1,
+        date: expect.any(Number),
+      }),
+    })
+  })
 })
